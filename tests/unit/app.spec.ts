1865e7a9e78087311e02c253c2aa58ef5b0926c5
Victor Araújo <48557502+Victorb999@users.noreply.github.com>
2021-03-16 18:37:25 -0300

    ✅ Add test dark theme

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,18 +1,35 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, flushPromises } from "@vue/test-utils";
 import App from "@/App.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "@/views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
 
 describe("App.vue", () => {
-  // it("test the dark theme", async () => {
-  //   const wrapper = shallowMount(App);
-  //   await wrapper.find('#checkbox').trigger('click');
+  it("test the dark theme", async () => {
+    router.push("/");
+    await router.isReady();
 
-  //   expect(wrapper.classes("theme-dark")).toBe(true);     
-  // });
-  
-   // arrange
-   //const wrapper = shallowMount(App)
+    const wrapper = shallowMount(App, {
+      global: {
+        plugins: [router]
+      }
+    });
+    await wrapper.find("#checkbox").trigger("click");
 
-   // assert
-  // expect(wrapper.html()).toMatchSnapshot()
-  it('works', () => {})
+    expect(wrapper.classes("theme-dark")).toBe(true);
+  });
+
 });
